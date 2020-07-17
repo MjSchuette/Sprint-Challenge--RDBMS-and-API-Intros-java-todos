f8159115ac2d87b99daeb3bf1ac734bdb0289495
MatthewSchuette <mjschuette4@gmail.com>
2020-07-17 12:51:30 -0500

    Resolved error getting close to done

--- todos/src/main/java/com/lambdaschool/todos/repository/TodoRepository.java
+++ todos/src/main/java/com/lambdaschool/todos/repository/TodoRepository.java
@@ -1,10 +1,7 @@
 package com.lambdaschool.todos.repository;
 
-
 import com.lambdaschool.todos.models.Todos;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TodoRepository extends CrudRepository<Todos, Long> {
 }
--- todos/src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
+++ todos/src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
@@ -1,22 +1,25 @@
 package com.lambdaschool.todos.services;
 
+import ch.qos.logback.core.boolex.EvaluationException;
 import com.lambdaschool.todos.models.Todos;
 import com.lambdaschool.todos.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 
+@Service(value = "todosService")
 public class TodosServiceImpl implements TodosService {
     @Autowired
-    TodoRepository todoRepos;
+    TodoRepository todosRepos;
 
     @Transactional
     @Override
     public void markComplete(long todoid) {
-        Todos todo = todoRepos.findById(todoid)
+        Todos todo = todosRepos.findById(todoid)
                 .orElseThrow(() -> new EntityNotFoundException("Customer " + todoid + " Not Found"));
         todo.setCompleted(true);
-        todoRepos.save(todo);
+        todosRepos.save(todo);
     }
 }
